3e511a0f5f3abac00c04e6af3535a24a3552e239
Uradouby <1468247858@qq.com>
2023-02-16 14:45:04 -0500

    finish testing

---
+++ app/src/test/java/com/example/flashcard/LogInModelTest.kt
@@ -0,0 +1,12 @@
+package com.example.flashcard
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+internal class LogInModelTest {
+
+    @Test
+    fun check() {
+    }
+}
---
+++ app/src/test/java/com/example/flashcard/flashcardModelTest.kt
@@ -0,0 +1,16 @@
+package com.example.flashcard
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+internal class flashcardModelTest {
+
+    @Test
+    fun isCorrect() {
+    }
+
+    @Test
+    fun getTotalScore() {
+    }
+}
